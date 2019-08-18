58254dcf1d35fd4773c6955364c922e66fd4ad19
Toshi <toshihog@Toshihog.local>
2019-08-18 15:17:12 +0800

    Create MemoList

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BodyText from './src/elements/BodyText';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarColor}>MEMO</Text>
+        </View>
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2019/08/31</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2019/08/31</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2019/08/31</Text>
+        </View>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2019/08/31</Text>
+        </View>
+      </View> 
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddButtonTitle}>+</Text>
+      </View>   
     </View>
   );
 }
@@ -12,8 +38,67 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:70,
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 35,
+    right: 35,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    textShadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 24,
+    lineHeight: 24,
+    color: '#fff'
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    height: 70,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    textShadowRadius: 3,
+    zIndex:10,
+  },
+  appbarColor : {
+    color: '#fff',
+    fontSize: 18,
   },
 });
---
+++ src/elements/BodyText.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native"
+
+class BodyText extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text style={styles.text}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#DDD'
+  }
+});
+
+export default BodyText;
