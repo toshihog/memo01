534204cade39d3d86fd818976f9b9d472873f38e
Toshi <toshihog@Toshihog.local>
2019-08-25 17:21:40 +0800

    Add Icon Font

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import { createIconSet } from '@expo/vector-icons';
+import fontAwsome from '../../assets/fonts/fa-solid-900.ttf';
+
+const CustomIcon = createIconSet({
+  pencil: '\uf303',
+  plus: '\uf067',
+}, 'FontAwsome');
 
 class CircleButton extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      FontAwsome: fontAwsome,
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     //const style = this.props.style;
-    const { style, color } = this.props;
+    const { style, color, name } = this.props;
 
     let bgColor = '#E31676';
     let textColor = '#fff';
@@ -16,9 +36,10 @@ class CircleButton extends React.Component {
 
     return(
       <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
-          <Text style={[styles.circleButtonTitle, {color: textColor}]}>
-            {this.props.children}
-          </Text>
+        { this.state.fontLoaded ? (
+          <CustomIcon name={this.props.name} style={[styles.circleButtonTitle, {color: textColor}]} />
+          ) : null
+        }
       </View>
     );
   }
@@ -40,6 +61,7 @@ const styles = StyleSheet.create({
     textShadowRadius: 3,
   },
   circleButtonTitle: {
+    fontFamily: 'FontAwsome',
     fontSize: 24,
     lineHeight: 24,
   },
