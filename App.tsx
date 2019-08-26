515980f3db7a43f859ef1a4f2b1fbbdccfa3be2d
Toshi <toshihog@Toshihog.local>
2019-08-26 20:45:02 +0800

    Create MemoEditScreen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import Appbar from './src/components/Appbar';
 
 export default class App extends React.Component {
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoDetailScreen />
+        <MemoEditScreen />
       </View>
     );
   }
